18744ddc2bc0e6da13554dc5938ad3fa02539407
Ryan <ryanrga01@gmail.com>
2022-12-22 21:42:50 +0700

    Menambah program

---
+++ Pertemuan ke-9/ProgramAbstract/src/Main.java
@@ -0,0 +1,9 @@
+import jenis.Himalaya;
+import jenis.Maincone;
+
+public class Main {
+    public static void main(String[] args) {
+        new Maincone("Ciko").kucing();
+        new Himalaya("Deno").kucing();
+    }
+}
---
+++ Pertemuan ke-9/SuperClass/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) {
+//        AdaYangSalah ays = new AdaYangSalah();
+//        AdaYangSalah.cetak1();
+//        AdaYangSalah.cetak2();
+//        ays.cetak3();
+
+        Perbaikan perbaik = new Perbaikan();
+        Perbaikan.cetak1();
+        perbaik.cetak2();
+        perbaik.cetak3();
+    }
+}
