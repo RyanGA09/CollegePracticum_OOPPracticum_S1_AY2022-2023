92a431d6e5ae67c4196ca3e338b015aa077b56f1
Ryan <ryanrga01@gmail.com>
2022-12-28 19:31:22 +0700

    Menambah Program

--- Project (Studi Kasus)/AplikasiBanking/src/view/frame/viewmainmenupage/GuiInfoAkunFrame.java
+++ Project (Studi Kasus)/AplikasiBanking/src/view/frame/viewmainmenupage/GuiInfoAkunFrame.java
@@ -1,4 +1,20 @@
 package view.frame.viewmainmenupage;
 
-public class GuiInfoAkunFrame {
+import view.frame.GuiPage;
+
+public class GuiInfoAkunFrame extends GuiPage {
+
+    public GuiInfoAkunFrame(){
+        super("INFO AKUN", 3, 4);
+    }
+
+    @Override
+    protected void component() {
+
+    }
+
+    @Override
+    protected void event() {
+
+    }
 }
